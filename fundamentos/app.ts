b8c889a91e427662373eae9877b34846ecd360b6
gustavo <gustavo.camilo@sou.unifal-mg.edu.br>
2024-02-14 12:19:04 -0300

    desafio 1

--- fundamentos/app.ts
+++ fundamentos/app.ts
@@ -1,42 +1,14 @@
-/*const user = {
-    name: "Gus",
-    age: 21,
-    showName(){
-        console.log(this.name)
-    }
-}
+import { PeopleAccount } from './class/PeopleAccount'
+import { CompanyAccount } from './class/CompanyAccount'
 
-const oherUser = {
-    name: "Lis",
-    age: 20,
-    showName(){
-        console.log(this.name)
-    }
-}
-user.showName()
-oherUser.showName()
+const PeopleAccount1: PeopleAccount = new PeopleAccount(1, 'Gus', 103)
+console.log(PeopleAccount1)
 
-*/
+const companyAccount: CompanyAccount = new CompanyAccount('Dio', 104)
+console.log(companyAccount)
 
-// Classes serve para criar objetos, a estrutura é a mesma de um objeto
-class User {
-    name: string = 'Gus'
-    age: number = 21
+PeopleAccount1.deposit()
+companyAccount.deposit()
 
-    // O construtor é um método que é chamado quando a classe é instanciada
-    // Palavra this é usada para acessar os atributos de um objeto, e acessar os métodos de um objeto
-    constructor(name: string, age: number){
-        this.name = name 
-        this.age = age
-    }
+PeopleAccount1.setName('Gustavo Fidelis')
 
-    showName = () => {
-        console.log(this.name)
-    }
-}
-
-const  user = new User('Gus', 21)
-user.showName() 
-
-const otherUser = new User('Lis', 20)
-otherUser.showName()
---
+++ fundamentos/class/CompanyAccount.ts
@@ -0,0 +1,18 @@
+import { DioAccount } from './DioAccount'
+
+export class CompanyAccount extends DioAccount {
+    constructor(name: string, accountNumber: number){
+        super(name, accountNumber)
+    }
+
+    getLoan = (): void => {
+        console.log('Você pegou um empréstimo')
+    }
+
+    //sobreescrita
+    deposit = (): void => {
+        console.log('A empresa depositou')
+    }
+
+
+}
